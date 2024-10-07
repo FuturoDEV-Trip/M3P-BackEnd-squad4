70620843f6047f3485e902520b0cb23084df2814
Rebeca Verissimo <rebeca_verissimo@estudante.sesisenai.org.br>
2024-10-07 14:32:06 -0500

    cep local requerido

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -37,9 +37,7 @@ const schemaAdicionarLocal = yup.object().shape({
   nome: yup.string().required(),
   descricao: yup.string().required(),
   localidade: yup.string().required(),
-  cep: yup.string()
-  .notRequired() //ajustado
-  .matches(/^\d{8}$/, "O CEP deve conter 8 dígitos sem pontos nem traços"),
+  cep: yup.string().matches(/\d{8}/, "O CEP deve conter 8 dígitos sem pontos nem traços").required(),
   coordenadas_geograficas: yup.string(),
 });
 
