062efb78a6aecd2377dd0972bee99c160ad7514f
a-natureza <97416824+a-natureza@users.noreply.github.com>
2024-10-11 19:42:06 -0300

    atualização database.config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,8 +2,8 @@ const { config } = require('dotenv');
 config();
 
 module.exports = {
-  dialect: process.env.DIALECT, // postgres
-  host: process.env.HOST, // seu host no render
+  dialect: process.env.DIALECT || 'postgres',
+  host: process.env.HOST,
   username: process.env.USERNAMEDB,
   password: process.env.PASSWORDDB,
   database: process.env.DATABASE,
