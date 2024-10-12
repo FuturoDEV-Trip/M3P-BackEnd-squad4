b8234c0b292bea254ddb4d78d830ac0b8d0bd574
a-natureza <97416824+a-natureza@users.noreply.github.com>
2024-10-11 21:51:38 -0300

    atualizando o server para corrigir Error: Cannot find module './src/database/connection'

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { connection } = require("./src/database/connection");
+const { connection } = require("./database/connection");
 const routes = require("./src/routes");
 
 const PORT_API = process.env.PORT_API || 3000;
